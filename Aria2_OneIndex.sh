f8c977694572d7d9b8e8c94404c87240822be06a
EdifierDrew <51026017+edifierdrew@users.noreply.github.com>
2022-03-25 09:35:02 +0800

    Update Aria2_OneIndex.sh

--- Aria2_OneIndex.sh
+++ Aria2_OneIndex.sh
@@ -90,7 +90,7 @@ php7_install(){
 	#该方法是使用第三方源添加php7.0
 	apt install apt-transport-https lsb-release ca-certificates
 	wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-	echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+	echo -e "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
 	#install php7.0
         apt install php7.0-cgi php7.0-fpm php7.0-curl php7.0-gd -y
         if [[ $? -eq 0 ]];then
